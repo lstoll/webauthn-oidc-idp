a125c44e3acfdd87b91c8b2a3909393f6399511e
Lincoln Stoll <me@lds.li>
2021-10-16 14:45:29 -0700

    add static and multi clients
    for webauthn we'll want to inject a client for the manager website, this will help

--- clients.go
+++ clients.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"regexp"
 
+	"github.com/pardot/oidc/core"
 	"gopkg.in/yaml.v2"
 )
 
@@ -25,28 +27,13 @@ type Client struct {
 	Public        bool     `json:"public" yaml:"public"`
 }
 
-type fsClients struct {
-	readerFn func() (io.ReadCloser, error)
+// staticClients is a clientsource that operates on a fixed list of clients.
+type staticClients struct {
+	clients []Client
 }
 
-func (f *fsClients) load() ([]Client, error) {
-	r, err := f.readerFn()
-	if err != nil {
-		return nil, fmt.Errorf("getting reader: %v", err)
-	}
-	var cs []Client
-	if err := yaml.NewDecoder(r).Decode(&cs); err != nil {
-		return nil, fmt.Errorf("decoding clients: %v", err)
-	}
-	return cs, nil
-}
-
-func (f *fsClients) IsValidClientID(clientID string) (ok bool, err error) {
-	cs, err := f.load()
-	if err != nil {
-		return false, err
-	}
-	for _, c := range cs {
+func (s *staticClients) IsValidClientID(clientID string) (ok bool, err error) {
+	for _, c := range s.clients {
 		if c.ClientID == clientID {
 			return true, nil
 		}
@@ -54,17 +41,12 @@ func (f *fsClients) IsValidClientID(clientID string) (ok bool, err error) {
 	return false, nil
 }
 
-func (f *fsClients) IsUnauthenticatedClient(clientID string) (ok bool, err error) {
+func (s *staticClients) IsUnauthenticatedClient(clientID string) (ok bool, err error) {
 	return false, nil
 }
 
-func (f *fsClients) ValidateClientSecret(clientID, clientSecret string) (ok bool, err error) {
-	cs, err := f.load()
-	if err != nil {
-		return false, err
-	}
-
-	for _, c := range cs {
+func (s *staticClients) ValidateClientSecret(clientID, clientSecret string) (ok bool, err error) {
+	for _, c := range s.clients {
 		if c.ClientID == clientID {
 			for _, cs := range c.ClientSecrets {
 				if cs == clientSecret {
@@ -76,15 +58,10 @@ func (f *fsClients) ValidateClientSecret(clientID, clientSecret string) (ok bool
 	return false, nil
 }
 
-func (f *fsClients) ValidateClientRedirectURI(clientID, redirectURI string) (ok bool, err error) {
-	cs, err := f.load()
-	if err != nil {
-		return false, err
-	}
-
+func (s *staticClients) ValidateClientRedirectURI(clientID, redirectURI string) (ok bool, err error) {
 	var cl Client
 	var found bool
-	for _, c := range cs {
+	for _, c := range s.clients {
 		if c.ClientID == clientID {
 			cl = c
 			found = true
@@ -104,4 +81,115 @@ func (f *fsClients) ValidateClientRedirectURI(clientID, redirectURI string) (ok 
 	}
 
 	return false, nil
+}
+
+// fsClients extends the static source to load the static list from a given
+// reader on each invocation
+//
+// TODO - consider caching at this level
+type fsClients struct {
+	readerFn func() (io.ReadCloser, error)
+}
+
+func (f *fsClients) load() (*staticClients, error) {
+	r, err := f.readerFn()
+	if err != nil {
+		return nil, fmt.Errorf("getting reader: %v", err)
+	}
+	sc := &staticClients{}
+	if err := yaml.NewDecoder(r).Decode(&sc.clients); err != nil {
+		return nil, fmt.Errorf("decoding clients: %v", err)
+	}
+	return sc, nil
+}
+
+func (f *fsClients) IsValidClientID(clientID string) (ok bool, err error) {
+	sc, err := f.load()
+	if err != nil {
+		return false, err
+	}
+	return sc.IsValidClientID(clientID)
+}
+
+func (f *fsClients) IsUnauthenticatedClient(clientID string) (ok bool, err error) {
+	return false, nil
+}
+
+func (f *fsClients) ValidateClientSecret(clientID, clientSecret string) (ok bool, err error) {
+	sc, err := f.load()
+	if err != nil {
+		return false, err
+	}
+	return sc.ValidateClientSecret(clientID, clientSecret)
+}
+
+func (f *fsClients) ValidateClientRedirectURI(clientID, redirectURI string) (ok bool, err error) {
+	sc, err := f.load()
+	if err != nil {
+		return false, err
+	}
+	return sc.ValidateClientRedirectURI(clientID, redirectURI)
+}
+
+// errSourceNotFound is returned by multiClients when no source handles the
+// client.
+var errSourceNotFound = errors.New("No source found for client ID")
+
+// multiClients is a clientsource that acts on a series of underlying client
+// sources. these are iterated in order, with the first client indicating a
+// client ID is valid being the source responsible for said client.
+type multiClients struct {
+	sources []core.ClientSource
+}
+
+func (m *multiClients) IsValidClientID(clientID string) (ok bool, err error) {
+	s, err := m.sourceForID(clientID)
+	if err != nil {
+		if err == errSourceNotFound {
+			// no source just means invalid client ID here
+			return false, nil
+		}
+		return false, err
+	}
+	return s.IsValidClientID(clientID)
+}
+
+func (m *multiClients) IsUnauthenticatedClient(clientID string) (ok bool, err error) {
+	s, err := m.sourceForID(clientID)
+	if err != nil {
+		return false, err
+	}
+	return s.IsUnauthenticatedClient(clientID)
+}
+
+func (m *multiClients) ValidateClientSecret(clientID, clientSecret string) (ok bool, err error) {
+	s, err := m.sourceForID(clientID)
+	if err != nil {
+		return false, err
+	}
+	return s.ValidateClientSecret(clientID, clientSecret)
+}
+
+func (m *multiClients) ValidateClientRedirectURI(clientID, redirectURI string) (ok bool, err error) {
+	s, err := m.sourceForID(clientID)
+	if err != nil {
+		return false, err
+	}
+	return s.ValidateClientRedirectURI(clientID, redirectURI)
+
+}
+
+// sourceForID finds the first source for which the client ID is valid, and
+// returns it. If no source found, an error is returned
+func (m *multiClients) sourceForID(clientID string) (core.ClientSource, error) {
+	for _, cs := range m.sources {
+		ok, err := cs.IsValidClientID(clientID)
+		if err != nil {
+			return nil, err
+		}
+		if ok {
+			return cs, nil
+		}
+	}
+	return nil, errSourceNotFound
 }
