1c0cd60fa275524c1c9ad5280f1f577c4de17c34
Lincoln Stoll <me@lds.li>
2022-05-28 16:41:13 +0200

    Remove unused config setup, dead functions

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"crypto/rsa"
-	"encoding/base64"
-	"encoding/gob"
 	"errors"
 	"fmt"
-	"log"
 	"os"
 	"runtime/debug"
-	"strconv"
 
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
@@ -82,42 +76,6 @@ func main() {
 	if runErr != nil {
 		l.WithError(runErr).Fatal()
 	}
-}
-
-func mustDecodeKey(s string) *rsa.PrivateKey {
-	var r *rsa.PrivateKey
-	b, err := base64.StdEncoding.DecodeString(s)
-	if err != nil {
-		log.Fatalf("un-base64 key: %v", err)
-	}
-	buf := bytes.Buffer{}
-	buf.Write(b)
-	if err := gob.NewDecoder(&buf).Decode(&r); err != nil {
-		log.Fatalf("decoding key: %v", err)
-	}
-	return r
-}
-
-var localDevKey = mustDecodeKey("S/+BAwEBClByaXZhdGVLZXkB/4IAAQQBCVB1YmxpY0tleQH/hAABAUQB/4YAAQZQcmltZXMB/4gAAQtQcmVjb21wdXRlZAH/igAAACT/gwMBAQlQdWJsaWNLZXkB/4QAAQIBAU4B/4YAAQFFAQQAAAAK/4UFAQL/kAAAABn/hwIBAQpbXSpiaWcuSW50Af+IAAH/hgAASP+JAwEBEVByZWNvbXB1dGVkVmFsdWVzAf+KAAEEAQJEcAH/hgABAkRxAf+GAAEEUWludgH/hgABCUNSVFZhbHVlcwH/jgAAAB3/jQIBAQ5bXXJzYS5DUlRWYWx1ZQH/jgAB/4wAADH/iwMBAQhDUlRWYWx1ZQH/jAABAwEDRXhwAf+GAAEFQ29lZmYB/4YAAQFSAf+GAAAA/gFB/4IBAUEC6ucB8ZXiGQZmcUaBfbEOGfYZoPcs32XGIgHCugePcP3G7cIc5DxofX0gV5lo11+DLDFVYmVDTq+YNYrPcr6LHQH9AgACAAFBAkChrvc5tiwMhsNEEvzyal7aR9LyL3aIGivhMCLfUahUpBlsA0C4DkqqcOTzKZI1dDIibFOTgEncrRPzDWikCkEBAiEC+8WMHSJDcR+Mw/I/bsslFBjMZYkJ7j8ph8MrBmqtfp8hAu7Y7vKhGiT8Xek9Foifb7k/I/5NNOOFr4jUDCyVyejDAQEhAucjVxywBgZmlo6VaVLHwQSQN6XHh4xoBDKVJHzBlwG1ASEC0NeuV0i2a5CfLMnVYjDGp9ulxT4M+MRz79g5rOJsYbEBIQIR0UGs8sDAlPOVpuFq3dFa0PROE4YBEQuqe4Rdb+UwpgAA")
-
-func getEnvOrDefaultStr(key, dfault string) string {
-	r := os.Getenv(key)
-	if r == "" {
-		return dfault
-	}
-	return r
-}
-
-func getEnvOrDefaultBool(key string, dfault bool) bool {
-	r := os.Getenv(key)
-	if r == "" {
-		return dfault
-	}
-	v, err := strconv.ParseBool(r)
-	if err != nil {
-		return false
-	}
-	return v
 }
 
 func getVersion() string {
