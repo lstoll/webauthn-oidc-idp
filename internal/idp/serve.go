119cf2fc08709f9121188bdf25b8c5a2b3bde1ad
Lincoln Stoll <me@lds.li>
2025-06-29 01:32:55 +0200

    Use the fly request ID header

--- internal/idp/serve.go
+++ internal/idp/serve.go
@@ -18,6 +18,7 @@ import (
 	"github.com/lstoll/oidc/discovery"
 	"github.com/lstoll/web"
 	"github.com/lstoll/web/csp"
+	"github.com/lstoll/web/requestid"
 	"github.com/lstoll/web/session"
 	"github.com/lstoll/web/session/sqlkv"
 	"github.com/lstoll/webauthn-oidc-idp/internal/queries"
@@ -71,7 +72,9 @@ func ServeCmd(ctx context.Context, sqldb *sql.DB, db *DB, issuerURL *url.URL, cl
 	if err != nil {
 		return fmt.Errorf("creating web server: %w", err)
 	}
-	// TODO - websvr should respect Fly-Request-ID
+	websvr.BaseMiddleware.Replace(web.MiddlewareRequestIDName, (&requestid.Middleware{
+		TrustedHeaders: []string{"Fly-Request-ID"},
+	}).Handler)
 
 	oidcmd := discovery.DefaultCoreMetadata(issuerURL.String())
 	oidcmd.AuthorizationEndpoint = issuerURL.String() + "/auth"
